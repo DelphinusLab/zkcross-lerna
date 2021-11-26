08d3afda4a040909d7fef5104c6d8d3411877d80
Heng Zhang <shindar90@gmail.com>
2021-11-27 02:11:07 +0800

    add start.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 [ -d "./packages" ] || mkdir packages
 
 cd packages
-repo init -u git@github.com:ZhenXunGe/delphinus.git -b main
+repo init -u git@github.com:ZhenXunGe/delphinus.git -b dev
 repo sync
 cd -
 
