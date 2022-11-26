8ea58318525c5e02bfc5756dea1d556051190f15
RuilaiZhang <ray.zhangruilai@gmail.com>
2022-11-26 15:52:30 +1100

    script to update verifier

--- update_verifier.sh
+++ update_verifier.sh
@@ -1,6 +1,7 @@
     cp packages/zkp/circom/verifier.sol packages/solidity/contracts/ZKPVerifier.sol
     sed -i "s/\^0.6.11/^0.8.0/" packages/solidity/contracts/ZKPVerifier.sol
 
+    # Dont have to migrate the tokens again
     cd packages/solidity
     npx truffle migrate --f 4 --to 4 --network goerli
     npx truffle migrate --f 4 --to 4 --network bsctestnet
@@ -8,6 +9,4 @@
     npx truffle migrate --f 4 --to 4 --network rolluxtestnet
     cd -
 
-    cd packages/solidity/clients/
-    node config-contracts-info.js
-    cd -
+    echo "new verifier has been updated"
